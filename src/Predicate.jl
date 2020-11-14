e7c5e5e452ca4b2faf4bcb312e9f85ec32e08dbc
Julian Stobbe <Atomtomate@gmx.de>
2020-11-14 17:59:15 +0100

    make_symmetric

--- src/Predicate.jl
+++ src/Predicate.jl
@@ -8,11 +8,35 @@ end
 
 # ====================  Auxiliary functions ====================
 
-function check_for_equivalence_relation(f::Predicate, domain)
-  for (i,el1) in enumerate(domain)
-    for el2 in domain[i+1:end]
+"""
+    check_for_equivalence_relation(f::Predicate, dom)
+
+Given a Predicate `f` over a domain `dom`, return `true` if `f`
+is a equivalence relation, `false` otherwise.
+One can create a symmetric equivalency relation with [`make_symmetric(f)`](@ref)
+
+# Examples
+```
+julia> check_for_equivalence_relation(Predicate((x,y) -> x == y), -11:10)
+true
+julia> check_for_equivalence_relation(Predicate((x,y) -> x == y+2), -1:10)
+false
+```
+"""
+function check_for_equivalence_relation(f::Predicate, dom)
+  for (i,el1) in enumerate(dom)
+    for el2 in dom[i+1:end]
       !(f(el1,el2) == f(el2,el1)) && return false
     end
   end
   return true
+end
+
+"""
+    make_symmetric(f::Predicate)
+
+Create symmetric predicate from arbitrary predicateg
+"""
+function make_symmetric(f::Predicate)
+  return f(x,y) || f(y,x)
 end
